5a96ee6470e0137ae3a38993221ca70d531eae36
Daniil <sezonov.daniil@gmail.com>
2019-10-18 19:11:34 +0300

    И вернул её в декоратор...

--- decorators.py
+++ decorators.py
@@ -1,18 +1,26 @@
 from rest_framework.exceptions import PermissionDenied
 
-_consumers = []
+from backends.remote_client_validator import get_client_by_host, get_client_by_uuid
+from backends.remote_client_validator.registred_clients import get_default_client
 
 
 def client_application_required(func, **kwargs):
-    _consumers.append(func)
 
     def wrapper(request):
-        if func.__name__ in [consumer.__name__ for consumer in _consumers]:
-            # Look at remote_client_validator.middleware
-            if not request.client:
-                raise PermissionDenied("The client is not allowed.")
-            request.client.current_consumer = func.__name__
-            return func(request)
+        client = None
+
+        app_uuid = request.META.get('HTTP_Application')
+
+        if app_uuid is None:
+            client = get_client_by_host(request.META.get('HTTP_Origin'))
+        else:
+            client = get_client_by_uuid(app_uuid)
+
+        request.client = client or get_default_client()
+
+        if not request.client:
+            raise PermissionDenied("The client is not allowed.")
+        return func(request)
 
     wrapper.__name__ = func.__name__
     return wrapper
